2794bcdaab8eaa153e3399be89e7db9d6c95f359
Benoit Blanchon <github@benoitblanchon.fr>
2016-01-26 22:00:40 +0100

    Update headers

--- include/ArduinoJson/Configuration.hpp
+++ include/ArduinoJson/Configuration.hpp
@@ -1,3 +1,10 @@
+// Copyright Benoit Blanchon 2014-2016
+// MIT License
+//
+// Arduino JSON library
+// https://github.com/bblanchon/ArduinoJson
+// If you like this project, please add a star!
+
 #pragma once
 
 #ifdef ARDUINO  // assume this is an embedded platform
